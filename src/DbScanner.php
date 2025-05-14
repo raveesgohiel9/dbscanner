34f7cbab7da3039d49cdd277112073df0004bba3
raveesgohiel <ravi_gohil999@yahoo.com>
2025-05-14 10:50:16 -0400

    Added the code to read table names in  the scan function

---
+++ src/DbScanner.php
@@ -0,0 +1,33 @@
+<?php
+namespace DbScanner;
+
+use Exception;
+use Illuminate\Support\Facades\Schema;
+
+class DbScanner {
+    
+    public function __construct(public $tables, public $views, private $db_report) {
+        $this->db_report = [];
+    }
+
+    public function scan() {
+        $this->tables = Schema::getTables();
+        //For Loop for the table goes here 
+
+    }
+
+    private function getTableIndexes($table_name = "") {
+        if (!empty($table_name)) {
+            $table_indexes = Schema::getIndexes($table_name);
+            return $table_indexes;
+        } 
+
+        return null;
+    }
+
+    public function getReport() {
+        return $this->db_report;
+    }
+}
+
+?>
